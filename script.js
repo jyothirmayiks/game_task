797f336365b0ce52c841e3b198c93230beb126c8
jyothirmayiks <jothirmayiks@gmail.com>
2025-02-11 14:05:07 +0530

    feat: add front page

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("startGame").addEventListener("click", function() {
+    window.location.href = "game.html"; 
+});
+
+document.getElementById("exitGame").addEventListener("click", function() {
+    window.close(); 
+});
